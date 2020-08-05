4787b3d34085d5c0fe1a2c177bd5ab41fc90b072
divyenth <60209291+divyenth@users.noreply.github.com>
2020-08-05 18:30:13 +0530

    Update sms.py

--- SMS/sms.py
+++ SMS/sms.py
@@ -2,14 +2,14 @@
 
 from twilio.rest import Client 
  
-account_sid = 'AC17bd454b57bb250d4c8e4d1d5169c677' 
-auth_token = '7451dfe0b564874f4da15e26b29bf199' 
+account_sid = 'Your account sid' 
+auth_token = 'Your account auth token' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
-                              from_='+12056560905',  
-                              body='haiii twilio ha some really good features..!',      
-                              to='+919159029500' 
+                              from_='Number given by Twilio',  
+                              body='message content',      
+                              to='to number eg:+911234567890' 
                           ) 
  
 print(message.sid)
